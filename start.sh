dcf8467e11aea2065da0156c2dfec9d574ffd328
sliploach <141513245+sliploach@users.noreply.github.com>
2024-07-21 21:45:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # 端口参数 （必填）
-export WEBPORT=13401
-export VMPORT=58630
-
+export WEBPORT=
+export VMPORT=
 # web.js 参数 （必填）
-export UUID=a884290a-fe19-4004-8b84-fdeac9d908d4
+export UUID=
 export WSPATH=serv00
 
 # ARGO 隧道参数（如需固定 ARGO 隧道，请把 ey 开头的 ARGO 隧道的 token 填入 ARGO_AUTH ，仅支持这一种方式固定，隧道域名代理的协议为 HTTP ，端口为 VMPORT 同端口。如果不固定 ARGO 隧道，请删掉ARGO_DOMAIN那行，保留ARGO_AUTH这行。）
